1fa0f19033b2d9a83ce5f7144a3be1d21135b982
prakharmoses <me21b054@iittp.ac.in>
2024-07-17 10:49:19 +0530

    Done

--- src/views/admin/rtl/index.jsx
+++ src/views/admin/rtl/index.jsx
@@ -58,115 +58,108 @@ import {
 import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
 import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
 
+import { useState } from "react";
+import {
+  FormControl,
+  Input,
+  Radio,
+  RadioGroup,
+  Stack,
+} from "@chakra-ui/react";
+
 export default function UserReports() {
   // Chakra Color Mode
+  // const brandColor = useColorModeValue("brand.500", "white");
+  // const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
   const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
-  return (
-    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
-        gap='20px'
-        mb='20px'>
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />
-              }
-            />
-          }
-          name='Earnings'
-          value='$350.4'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />
-              }
-            />
-          }
-          name='Spend this month'
-          value='$642.39'
-        />
-        <MiniStatistics growth='+23%' name='Sales' value='$574.34' />
-        <MiniStatistics
-          endContent={
-            <Flex me='-16px' mt='10px'>
-              <FormLabel htmlFor='balance'>
-                <Avatar src={Usa} />
-              </FormLabel>
-              <Select
-                id='balance'
-                variant='mini'
-                mt='5px'
-                me='0px'
-                defaultValue='usd'>
-                <option value='usd'>USD</option>
-                <option value='eur'>EUR</option>
-                <option value='gba'>GBA</option>
-              </Select>
-            </Flex>
-          }
-          name='Your balance'
-          value='$1,000'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-              icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
-            />
-          }
-          name='New Tasks'
-          value='154'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />
-              }
-            />
-          }
-          name='Total Projects'
-          value='2935'
-        />
-      </SimpleGrid>
 
-      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
-        <TotalSpent />
-        <WeeklyRevenue />
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <DailyTraffic />
-          <PieCard />
-        </SimpleGrid>
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <ComplexTable
-          columnsData={columnsDataComplex}
-          tableData={tableDataComplex}
+  const [gameName, setGameName] = useState("");
+  const [gameLink, setGameLink] = useState("");
+  const [gameDescription, setGameDescription] = useState("");
+  const [gameVideoLink, setGameVideoLink] = useState("");
+  const [ticketPrice, setTicketPrice] = useState("");
+  const [isMultiplayer, setIsMultiplayer] = useState("");
+
+  const handleGameNameChange = (event) => {
+    setGameName(event.target.value);
+  };
+
+  const handleGameLinkChange = (event) => {
+    setGameLink(event.target.value);
+  };
+
+  const handleGameDescriptionChange = (event) => {
+    setGameDescription(event.target.value);
+  };
+
+  const handleGameVideoLinkChange = (event) => {
+    setGameVideoLink(event.target.value);
+  };
+
+  const handleTicketPriceChange = (event) => {
+    setTicketPrice(event.target.value);
+  };
+
+  const handleIsMultiplayerChange = (event) => {
+    setIsMultiplayer(event.target.value);
+  };
+  return (
+    <main>
+      <FormControl>
+        <FormLabel>Game Name</FormLabel>
+        <Input
+          type="text"
+          value={gameName}
+          onChange={handleGameNameChange}
         />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <Tasks />
-          <MiniCalendar h='100%' minW='100%' selectRange={false} />
-        </SimpleGrid>
-      </SimpleGrid>
-    </Box>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Game Link</FormLabel>
+        <Input
+          type="text"
+          value={gameLink}
+          onChange={handleGameLinkChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Game Description</FormLabel>
+        <Input
+          type="text"
+          value={gameDescription}
+          onChange={handleGameDescriptionChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Game Video Link</FormLabel>
+        <Input
+          type="text"
+          value={gameVideoLink}
+          onChange={handleGameVideoLinkChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Ticket Price</FormLabel>
+        <Input
+          type="text"
+          value={ticketPrice}
+          onChange={handleTicketPriceChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel>Is Multiplayer?</FormLabel>
+        <RadioGroup value={isMultiplayer} onChange={handleIsMultiplayerChange}>
+          <Stack direction="row">
+            <Radio value="yes">Yes</Radio>
+            <Radio value="no">No</Radio>
+          </Stack>
+        </RadioGroup>
+      </FormControl>
+    </main>
   );
 }
